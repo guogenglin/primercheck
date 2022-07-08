cb619ca422178cd3a8a920a97ac130e3ef9a0516
guogenglin <108860907+guogenglin@users.noreply.github.com>
2022-07-08 14:47:09 +0800

    Add files via upload

---
+++ primercheck.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+#prepare the primer file
+#所需软件：samtools >= 1.9 BLAST+, primerserver2, tsv-utils
+#tsv-utils添加到PATH
+ls *.fna | while read id; do (samtools faidx $id); done
+ls *.fna | while read id; do (makeblastdb -in $id -dbtype nucl); done
+ls *.fna | while read id; do (primertool check primer.txt $id -t ${id%%.fna}.tsv); done
+cat *.tsv > processing.tsv
+tsv-filter -H --is-numeric 1 --ge 1:1 processing.tsv > result.01.tsv
+tsv-select -f 1,12-15 result.01.tsv > result.02.tsv
+tsv-filter -H --is-numeric Possible_Amplicon_Number --ge Possible_Amplicon_Number:1 result.02.tsv > result.03.tsv
+
